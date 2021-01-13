6ced2bee08633f8e11336382f4f14daf299fc5fb
Dianana-k <70758831+Dianana-k@users.noreply.github.com>
2021-01-14 00:32:34 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+$(function () {
+    const $win = $(window);
+    const $btnMenu = $('.btn-menu');
+    const $nav = $('header > nav');
+    const isAnimate = 'is-animate';
+    const isOpen = 'is-open';
+//ボタンをクリックしたときの記述
+
+$btnMenu.on('click', function () {
+  
+const $this = $(this);
+if ($nav.is(':animated')) {
+    return;
+}
+if (!$this.hasClass(isOpen)) {
+    $nav.slideDown();
+    $this.addClass(isOpen).addClass(isAnimate);
+} else {
+    $nav.slideUp();
+    $this.removeClass(isOpen).removeClass(isAnimate);
+}
+});
+//ウィンドウ幅を変えた時の記述
+$win.on('resize', function () {
+if ($win.width() > 800) {
+    $nav.show();
+    $btnMenu.removeClass(isOpen).removeClass(isAnimate);
+} else {
+    if (!$btnMenu.hasClass(isOpen)) {
+    $nav.hide();
+    }
+}
+});
+
+});
